bokwoon95/wgo

commit b92c7577df108c3ff9480df7079325153b6587b3

remove cmdDone atomic boolean, just close the cmdResult channel

wgo_cmd.go
@@ -17,7 +17,6 @@ import (
 	"strconv"
 	"strings"
 	"sync"
-	"sync/atomic"
 	"time"
 	"unicode/utf8"
 
@@ -421,7 +420,6 @@ func (wgoCmd *WgoCmd) Run() error {
 			}
 
 			// Step 2: Run the command in the background.
-			var cmdDone int32
 			cmdResult := make(chan error, 1)
 			err = cmd.Start()
 			if err != nil {
@@ -430,7 +428,7 @@ func (wgoCmd *WgoCmd) Run() error {
 			go func() {
 				wg.Wait()
 				cmdResult <- cmd.Wait()
-				atomic.StoreInt32(&cmdDone, 1)
+				close(cmdResult)
 			}()
 
 			// Step 3: Wait for events in the event loop.
@@ -438,9 +436,7 @@ func (wgoCmd *WgoCmd) Run() error {
 				select {
 				case <-wgoCmd.ctx.Done():
 					stop(cmd)
-					if atomic.LoadInt32(&cmdDone) == 0 {
-						<-cmdResult
-					}
+					<-cmdResult
 					return nil
 				case err := <-cmdResult:
 					if i == len(wgoCmd.ArgsList)-1 {
@@ -474,9 +470,7 @@ func (wgoCmd *WgoCmd) Run() error {
 					}
 				case <-timer.C: // Timer expired, reload commands.
 					stop(cmd)
-					// I cannot figure out why it's incorrect to wait for the
-					// cmd to finish here. Everytime I do, the program seems to
-					// freeze indefinitely.
+					<-cmdResult
 					break CMD_CHAIN
 				}
 			}

commit 6ea3e5229882b3068841dcdbdbed3d424825443b

fix chained commands proceeding even on error
Regression introduced in b92c757: the atomic bool was erroneously
replaced with closing the cmdResult channel. When the cmdResult channel
is closed, subsequent reads will keep yielding the zero value (a nil
error in this case). Therefore failed commands which should rightfully
break the chain end up proceeding anyway because first it fails, then it
"succeeds" (because select{} on the closed channel produces a zero
value). Fixed by introducing an additional channel waitDone which is
used to actually signal when a command is done.

wgo_cmd.go
@@ -434,6 +434,7 @@ func (wgoCmd *WgoCmd) Run() error {
 
 			// Step 2: Run the command in the background.
 			cmdResult := make(chan error, 1)
+			waitDone := make(chan struct{})
 			err = cmd.Start()
 			if err != nil {
 				return err
@@ -441,7 +442,7 @@ func (wgoCmd *WgoCmd) Run() error {
 			go func() {
 				wg.Wait()
 				cmdResult <- cmd.Wait()
-				close(cmdResult)
+				close(waitDone)
 			}()
 
 			// Step 3: Wait for events in the event loop.
@@ -449,7 +450,7 @@ func (wgoCmd *WgoCmd) Run() error {
 				select {
 				case <-wgoCmd.ctx.Done():
 					stop(cmd)
-					<-cmdResult
+					<-waitDone
 					return nil
 				case err := <-cmdResult:
 					if i == len(wgoCmd.ArgsList)-1 {
@@ -483,7 +484,7 @@ func (wgoCmd *WgoCmd) Run() error {
 					}
 				case <-timer.C: // Timer expired, reload commands.
 					stop(cmd)
-					<-cmdResult
+					<-waitDone
 					break CMD_CHAIN
 				}
 			}

wgo_cmd_test.go
@@ -409,7 +409,7 @@ func TestWgoCommands(t *testing.T) {
 			"wgo", "-debounce", "10ms", "echo", "test",
 		},
 		wantCmds: []*WgoCmd{{
-			Roots:       []string{"."},
+			Roots: []string{"."},
 			ArgsList: [][]string{
 				{"echo", "test"},
 			},